3e6f22f1154a78a8696caf7acab2c2da5afc5e94
Lev Illouz <156964356+levillouz@users.noreply.github.com>
2024-04-03 16:03:49 +0000

    Lab 11 Complete

--- script.js
+++ script.js
@@ -4,60 +4,59 @@ COMP 125 Lab 11 - Adding to an Array with mouse clicks
 Step 1 - add a line of code to function mousePressed() that will add a new object to the array when the mouse is pressed. The first part (creating a new object where the mouse is pressed) is done for you. Just add the line that appends it to the array.
 Step 2 - add comments explaining each line of code in this sketch.
 */
-var dots = new Array(1);
+var dots = new Array(1); //create a new array
 
-function setup() {
-  createCanvas(500, 300); 
-  dots[0] = new Dot(width/2, height/2);
+function setup() { //runs once
+  createCanvas(500, 300); // create a p5 canvas
+  dots[0] = new Dot(width/2, height/2); // initial location of first array at center
 }
 
 function draw() {
-  background(200);
+  background(200); // draws grey background
   
-  for(let i = 0; i < dots.length; i++){
-    dots[i].move(); 
-    dots[i].display(); 
+  for(let i = 0; i < dots.length; i++){ // repeat through each dot in the dots array
+    dots[i].move(); // moves the dot
+    dots[i].display(); // displays the dot
   }
-  textSize(24);
-  fill(100, 0, 200);
-  text("'dots' array length: " + dots.length, 100, 100);
+  textSize(24); // creates text on the canvas
+  fill(100, 0, 200); // makes text purple
+  text("'dots' array length: " + dots.length, 100, 100); // text content
 
 }
 
-function mousePressed(){
-  let obj = new Dot(mouseX, mouseY);
-  
-  /* add a line of code that adds "obj" to the "dots" array. Use the .push() method of the dots array object to append the new "obj" to the end of an existing array. Check the Array.push() documentation first to make sure you are using it correctly.
-  */
+function mousePressed(){ // function called when mouse is pressed
+  let obj = new Dot(mouseX, mouseY); // creates new dot at position of mouse
+  dots.push(obj); // creates new dot with each push of mouse
 
 }
 
 
-function Dot(X, Y){
+function Dot(X, Y){ // constructor for creating dots
   
-  this.x = X;
-  this.y = Y;
-  this.w = random (20, 50);
-  this.sx = random(-5, 5);
-  this.sy = random(-5, 5);
-  this.r = random(0, 255);
-  this.g = random(0, 255);
-  this.b = random(0, 255);
+  this.x = X; // x coordinate of dot
+  this.y = Y; // y coordinate of dot
+  this.w = random (20, 50); // dot width
+  this.sx = random(-5, 5); // speed of dot in x direction
+  this.sy = random(-5, 5); // speed of dot in y direction
+  this.r = random(0, 255); // red dot
+  this.g = random(0, 255); // green dot
+  this.b = random(0, 255); // blue dot
   
-  this.display = function(){
-    fill(this.r, this.g, this.b);
-    ellipse(this.x, this.y, this.w, this.w);
+  this.display = function(){ // display the dot
+    fill(this.r, this.g, this.b); // sets fill color to dot color
+    ellipse(this.x, this.y, this.w, this.w); // draw the dot as an ellipse at its position with width and height equal to 'w'
   }
   
-  this.move = function(){
-    this.x += this.sx;
-    this.y += this.sy;
+  this.move = function(){ // moves dot
+    this.x += this.sx; // update the x-coordinate based on the x-speed
+    this.y += this.sy; // update the y-coordinate based on the y-speed
     
-    if (this.x < 0 || this.x > width){
-        this.sx *= -1;
+    if (this.x < 0 || this.x > width){ // if the dot goes beyond canvas boundaries horizontally
+        this.sx *= -1; // reverse its x-speed to make it bounce
     }
-    if (this.y < 0 || this.y > height){
-        this.sy *= -1;
+    if (this.y < 0 || this.y > height){ // if the dot goes beyond canvas boundaries vertically
+
+        this.sy *= -1; // reverse its yspeed to make it bounce
     }
   }
     
